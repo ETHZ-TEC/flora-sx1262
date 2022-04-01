24e16e3ef5f0ff22d129b95b3cef416538d10d14
Roman Trueb <rtrueb@ethz.ch>
2022-04-01 16:37:59 +0200

    added matplotlib plotting

--- energy_per_bit/energy_per_bit.py
+++ energy_per_bit/energy_per_bit.py
@@ -39,14 +39,30 @@ import sys
 import numpy as np
 import pandas as pd
 
-from bokeh.io import output_file, show
+from bokeh.io import output_file, show, save
 from bokeh.models import ColumnDataSource, HoverTool, LinearColorMapper, CategoricalColorMapper
 from bokeh.palettes import plasma
 from bokeh.plotting import figure
 from bokeh.transform import transform
 
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+
 sys.path.append("..") # Adds higher directory to python modules path.
 from sx1262 import *
+
+
+################################################################################
+# Utils
+################################################################################
+def mapModulationToText(modulation):
+  mod, dr = modulation.split('_')
+  if mod == 'fsk':
+    return 'FSK {:0.0f} kbps'.format(float(dr))
+  elif mod == 'lora':
+    return 'LoRa SF{}'.format(int(dr.replace('sf', '')))
+  else:
+    raise Exception('Unknown modulation \'{}\'!'.format(mod))
 
 
 ################################################################################
@@ -165,10 +181,11 @@ def generateBorderPoints(pathlossModel, modulationList, configPwrList, payloadSi
             energyPerBit = energy/(payloadSize*8)
             actualBitrate = (payloadSize*8)/toa
 
+            linkBudget = configPwr - sensitivity
             if pathlossModel == 'friis':
-                distance = friis_pathlossToDistance(configPwr - sensitivity)
+                distance = friis_pathlossToDistance(linkBudget)
             elif pathlossModel == 'hata':
-                distance = hata_pathlossToDistance(configPwr - sensitivity)
+                distance = hata_pathlossToDistance(linkBudget)
             else:
                 raise Exception('You need to choose a valid pathloss model (pathlossModel)!')
 
@@ -179,6 +196,7 @@ def generateBorderPoints(pathlossModel, modulationList, configPwrList, payloadSi
                 'distance': distance,
                 'actualBitrate': actualBitrate,
                 'toa': toa,
+                'linkBudget': linkBudget,
             }
             retList.append(ret)
 
@@ -287,6 +305,10 @@ def plotPoints(pointSelection, pathlossModel, modulationList, configPwrList, pay
         ('actualBitrate', '@actualBitrate{int} bps'),
         ('Time-on-air', '@toa{0.0000} s'),
     ])
+    # add link budget (only available if pointSelection == 'border')
+    if pointSelection == 'border':
+        source.add(data=df.linkBudget, name='linkBudget')
+        hover.tooltips.append( ('linkBudget', '@linkBudget dB') )
     # mapper = LinearColorMapper(palette=plasma(256), low=5, high=12)
     mapper = CategoricalColorMapper(factors=modulationList, palette=plasma(len(modulationList)))
 
@@ -309,7 +331,111 @@ def plotPoints(pointSelection, pathlossModel, modulationList, configPwrList, pay
     p.legend.location = 'top_right'
 
     output_file('energyPerBit_{}_{}.html'.format(pointSelection, pathlossModel))
-    show(p)
+    save(p)
+    # show(p)
+
+    # matplotlib plots #####################################################
+    # these plots only work for border points!
+    if pointSelection != 'border':
+        return
+
+    plt.close('all')
+
+    # add modulation scheme to df
+    df['modScheme'] = [s.split('_')[0] for s in df.modulation]
+
+    # pareto front
+    dominated = [np.any(np.logical_and(row['linkBudget'] < df.linkBudget.to_numpy(), row['energyPerBit'] > df.energyPerBit.to_numpy())) for idx, row in df.iterrows()]
+    df['undominated'] = np.logical_not(dominated)
+    dfPareto = df[df.undominated]
+    dfParetoSorted = dfPareto.sort_values(by=['linkBudget'])
+
+    # sort df according to actualBitrate in order to get legend from left to right
+    df.sort_values(by=['modScheme', 'actualBitrate'], ascending=[True, False], inplace=True)
+
+    ## matplotlib: energy per bit vs. link budget/range
+    fig, ax1 = plt.subplots(figsize=(8, 4.8))
+    # colors = iter(cm.gist_rainbow(np.linspace(0, 1, len(df.modulation.unique()))))
+    # NOTE: markers that consists of a line only are not visible when facecolors='none'
+    markers = iter([".", "o", "v", "^", "<", ">", "8", "s", "p", "P", "*", "H", "D", "d"])
+    # markers = iter([".", ",", "o", "v", "^", "<", ">", "1", "2", "3", "4", "8", "s", "p", "P", "*", "h", "H", "+", "x", "X", "D", "d", "|", "_"])
+    for modulation, gdf in df.groupby('modulation'):
+      c = 'blue' if 'fsk' in modulation else 'orange'
+      # ax1.scatter(gdf.energyPerBit*1e6, gdf.distance*1e-3, color=next(colors), label=modulation)
+      ax1.scatter(gdf.energyPerBit*1e6, gdf.linkBudget, color=c, marker=next(markers), label=modulation, facecolors='none')
+    paretoLabel = 'Pareto front'
+    ax1.step(dfParetoSorted.energyPerBit*1e6, dfParetoSorted.linkBudget, c='k', label=paretoLabel, where='post')
+    ax1.set_xscale('log')
+    # ax1.set_yscale('log') # for distance plotting
+    ax1.grid(color='k', alpha=0.1, linestyle='--', linewidth=1.5)
+    ax1.set_xlabel('Energy per bit [$\mathrm{\mu J}$]')
+    # ax1.set_ylabel('Distance [m]')
+    ax1.set_ylabel('Link budget [dB]')
+
+    # sort legend items
+    handles, labels = plt.gca().get_legend_handles_labels()
+    paretoIdx = labels.index(paretoLabel)
+    order = [labels.index(mod) for mod in df.modulation.unique()]
+    ax1.legend(
+      [handles[paretoIdx]] + [handles[idx] for idx in order],
+      [labels[paretoIdx]] + [mapModulationToText(labels[idx]) for idx in order],
+      loc='center left',
+      bbox_to_anchor=(1.1, 0.5),
+    )
+
+    # add distance as separate axis
+    ax2 = ax1.twinx()
+    mn, mx = ax1.get_ylim()
+    ax2.set_ylim(friis_pathlossToDistance(mn)*1e-3, friis_pathlossToDistance(mx)*1e-3)
+    ax2.set_yscale('log')
+    ax2.set_ylabel('Range (Friis free-space) [km]')
+
+    # txPower annotation
+    sf12Df = df[df.modulation == df.modulation.to_list()[-1]]
+    sf12Map = dict(zip(sf12Df.configPwr, zip(sf12Df.energyPerBit*1e6, sf12Df.linkBudget)))
+    offset = 5
+    for configPwr in [-9, 0, 10, 20, 22]:
+      configPwrStr = str(configPwr) if configPwr <= 0 else '+{}'.format(configPwr)
+      ax1.annotate('{} dBm'.format(configPwrStr), sf12Map[configPwr], textcoords='offset points', xytext=(offset, -offset))
+    ax1.annotate('Tx power', sf12Map[-9], textcoords='offset points', xytext=(offset, -5*offset))
+    # print(sf12Map)
+
+    # stretch plot to fit annotation and legend
+    # ax1.margins(x=0.2) # scales both sides
+    ax1.set_xlim(0.3, 17e3)
+
+    plt.savefig('energyPerBit.pdf', bbox_inches="tight")
+
+
+    ## matplotlib: time-on-air vs. actual bit rate
+    fig, ax1 = plt.subplots(figsize=(8, 4.8))
+    markers = iter([".", "o", "v", "^", "<", ">", "8", "s", "p", "P", "*", "H", "D", "d"])
+    # markers = iter([".", ",", "o", "v", "^", "<", ">", "1", "2", "3", "4", "8", "s", "p", "P", "*", "h", "H", "+", "x", "X", "D", "d", "|", "_"])
+    for modulation, gdf in df.groupby('modulation'):
+      c = 'blue' if 'fsk' in modulation else 'orange'
+      # all toa and bit rate values should be equal for the same modulation
+      if np.sum(np.diff(gdf.toa)) != 0:
+        raise Exception('timeOnAir values are not equal for modulation {}: {}'.format(modulation, gdf.toa.to_numpy()))
+      if np.sum(np.diff(gdf.actualBitrate)) != 0:
+        raise Exception('actualBitrate values are not equal for modulation {}: {}'.format(modulation, gdf.actualBitrate.to_numpy()))
+      ax1.scatter(gdf.toa.to_numpy()[0]*1e3, gdf.actualBitrate.to_numpy()[0], color=c, marker=next(markers), label=modulation, facecolors='none')
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    ax1.grid(color='k', alpha=0.1, linestyle='--', linewidth=1.5)
+    ax1.set_xlabel('Time-on-air [ms]')
+    ax1.set_ylabel('Actual bit rate [bit/s]')
+
+    # sort legend items
+    handles, labels = plt.gca().get_legend_handles_labels()
+    order = [labels.index(mod) for mod in df.modulation.unique()]
+    ax1.legend(
+      [handles[idx] for idx in order],
+      [mapModulationToText(labels[idx]) for idx in order],
+      loc='center left',
+      bbox_to_anchor=(1.0, 0.5),
+    )
+
+    plt.savefig('time-on-air.pdf', bbox_inches="tight")
 
 ################################################################################
 # Main
@@ -317,10 +443,10 @@ def plotPoints(pointSelection, pathlossModel, modulationList, configPwrList, pay
 
 if __name__ == "__main__":
     # Config
-    modulationList = ['fsk_1', 'fsk_5', 'fsk_10', 'fsk_100', 'fsk_300', 'lora_sf5', 'lora_sf6', 'lora_sf7', 'lora_sf8', 'lora_sf9', 'lora_sf10', 'lora_sf11', 'lora_sf12'] # valid options 'lora_sfX' or 'fsk_Y' (X=spreading factor number, Y=bitrate in kbit)
+    modulationList = ['fsk_001', 'fsk_005', 'fsk_010', 'fsk_050', 'fsk_100', 'fsk_300', 'lora_sf05', 'lora_sf06', 'lora_sf07', 'lora_sf08', 'lora_sf09', 'lora_sf10', 'lora_sf11', 'lora_sf12'] # valid options 'lora_sfX' or 'fsk_Y' (X=spreading factor number, Y=bitrate in kbit)
     configPwrList = [-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
     payloadSize = 20 # in Bytes
-    pointSelection = 'reachable' # 'border' (max distance) or 'reachable' (reachable configuration)
+    pointSelection = 'border' # 'border' (max distance) or 'reachable' (reachable configuration)
     pathlossModel = 'friis' # 'friis' (Friis free-space pathloss model) or 'hata' (Hata sub-urban path loss model)
     numPoints = 10000
 
